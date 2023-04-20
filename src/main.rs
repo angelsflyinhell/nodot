8489289da78f7971621bcc103767096bbbfe4260
yuki <40737538+angelsflyinhell@users.noreply.github.com>
2023-04-20 09:55:23 +0200

    Remove rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
         let entry = entry.unwrap();
         if entry.file_type().is_file() {
             let file = entry.file_name().to_str().unwrap();
-            if file.starts_with("._") || file.ends_with(".rs"){
+            if file.starts_with("._") {
                 println!("Found file: {}", file);
             }
         }
