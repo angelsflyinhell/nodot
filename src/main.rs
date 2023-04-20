ffd8a0a860cccf8c409053e49acd984a879c5ebd
angelsflyinhell <azami@namespace.media>
2023-04-20 19:13:44 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use walkdir;
 
 fn main() {
-    // recursively scan folder
-    for entry in walkdir::WalkDir::new("src") {
+    for entry in walkdir::WalkDir::new("D:\\Music\\Samplepacks\\") {
         let entry = entry.unwrap();
         if entry.file_type().is_file() {
             let file = entry.file_name().to_str().unwrap();
             if file.starts_with("._") {
                 println!("Found file: {}", file);
+                std::fs::remove_file(entry.path()).unwrap();
+                println!("Removed file: {}", file);
             }
         }
     }
